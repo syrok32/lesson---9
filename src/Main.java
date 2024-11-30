cae2057a5c0aa7d72a052af23e13a1af465c323b
syrok32 <stepstepan2@gmail.com>
2024-11-30 23:06:33 +0300

    a

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+    }
+}
